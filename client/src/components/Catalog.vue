31fd0d72ca8a03cd876a9a06c3d13f941b180635
danoleg <doncooleg@gmail.com>
2022-09-16 00:09:11 +0300

    fix init map

--- client/src/components/Catalog.vue
+++ client/src/components/Catalog.vue
@@ -175,6 +175,12 @@ export default {
       axios.post('http://127.0.0.1:8011/data/getting', this.formdata).then(
           function (response) {
             that.items = response.data.data;
+            that.mapVisible = false;
+
+            that.$nextTick(() => {
+              // Adding the component back in
+              that.mapVisible = true;
+            });
           });
     },
     setData(item) {
